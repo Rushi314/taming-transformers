1f553d45c6d3823765a04365b7a5b271e939bc21
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2022-04-27 10:06:05 +0530

    Modified resize script, reverted ngpu change

--- taming/data/resize_data.py
+++ taming/data/resize_data.py
@@ -1,19 +1,36 @@
-import PIL
 import os
 from PIL import Image
+import shutil
 
-os.system("mkdir /work/rdudhat_umass_edu/taming-transformers/data/flickr10k_segmentations_256")
-fw = r'/work/rdudhat_umass_edu/taming-transformers/data/flickr10k_segmentations_256'
-fr = r'/work/rdudhat_umass_edu/taming-transformers/data/flickr10k_segmentations'
-for dirc in os.listdir(fr):
-    if dirc in ['.DS_Store']:
-        continue
-    n_dirc = fw+'/'+dirc
-    os.system("mkdir "+n_dirc)
-    print(n_dirc)
-    for file in os.listdir(fr+'/'+dirc):
-        f_img_read = fr+'/'+dirc+"/"+file
-        f_img_save = n_dirc+"/"+file
-        img = Image.open(f_img_read)
-        img = img.resize((256,256), Image.NEAREST)
-        img.save(f_img_save)
+TAMING_ROOT = f"{os.path.abspath(os.path.dirname(__file__))}/../.."
+# Parameters
+SIZE = 256
+PREFIX = "flickr10k"
+
+if __name__ == "__main__":
+    modes = ["images", "segmentations"]
+    for mode in modes:
+        dir = f"{PREFIX}_{mode}"
+        dirw = f"{PREFIX}{SIZE}_{mode}"
+        fr = os.path.join(TAMING_ROOT, "data", dir)
+        print(f"Reading {dir}...")
+        fw = os.path.join(TAMING_ROOT, "data", dirw)
+        os.makedirs(fw, exist_ok=True)
+        for dirc in os.listdir(fr):
+            if dirc in ['.DS_Store']:
+                continue
+            fw_dirc = os.path.join(fw, dirc)
+            os.makedirs(fw_dirc, exist_ok=True)
+            print(fw_dirc)
+            fr_dirc = os.path.join(fr, dirc)
+            for file in os.listdir(fr_dirc):
+                f_img_read = os.path.join(fr_dirc, file)
+                f_img_save = os.path.join(fw_dirc, file)
+                img = Image.open(f_img_read)
+                img = img.resize((256, 256), Image.NEAREST)
+                img.save(f_img_save)
+    shutil.copyfile(
+        os.path.join(TAMING_ROOT, "data", f"{PREFIX}_examples.txt"),
+        os.path.join(TAMING_ROOT, "data", f"{PREFIX}{SIZE}_examples.txt")
+    )
+    print("Done")
